308ec9399300930e844a1e57fd211764c65fe5df
Gustavo Robles <gustavo.robles@crowdint.com>
2014-03-11 20:14:07 -0600

    new repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Boliplastcms::Application.config.secret_key_base = 'f19e7dab218d36d36d39bef704e1de17b4c791fc5d00face804e17c14a372e39bec49252d95b2e5f6748b95b245f8e3b9d31219ed189255f644e8d641ad74bc2'
